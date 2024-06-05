a511e5d69e11c83758814722b74cf17f61f89c2c
Prashant <unisysglobalz@gmail.com>
2024-06-05 21:20:29 +0530

    Updated RCC  process

---
+++ src/day1/tests/RI_Slum_RCC_Connection.java
@@ -0,0 +1,86 @@
+package day1.tests;
+
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import day1.base.TestBase;
+import day1.page.HomePage;
+import day1.page.Login;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class RI_Slum_RCC_Connection {
+    WebDriver driver = null;
+    Login loginPg = null;
+    HomePage homePage = null;
+    ExtentReports report;
+    ExtentReports extent = new ExtentReports();
+
+
+@Test
+    public void createConnection() throws InterruptedException {
+        //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        driver = new TestBase().initialization();
+        loginPg = PageFactory.initElements(driver, Login.class);
+      driver.get(TestBase.prop.getProperty("url"));
+    ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
+    extent.attachReporter(spark);
+    ExtentTest test = extent.createTest("Test to Create new R1 Slum RCC Type Connection");
+        homePage = loginPg.login("swengp@gmail.com","Saltoris@123");
+        //Assert.assertTrue(homePage.verifyHomePageLoaded());
+        homePage.createNewConnectionR1SlumRCC();
+    test.pass("Connection applied R1 Slum hut");
+        driver.get(TestBase.prop.getProperty("admin_url"));
+        homePage = loginPg.login("cso01@veolia.com","Cso@123");
+        homePage.approveConnection("CSO");
+    test.pass("Approved By CSO");
+        homePage = loginPg.login("tl01@veolia.com","Tl@123");
+        homePage.approveConnection("TL");
+    test.pass("Approved By TL");
+        homePage = loginPg.login("mr01c@veolia.com","Mr@123");
+        homePage.approveConnection("MR");
+    test.pass("Approved By MR");
+        homePage = loginPg.login("tl01@veolia.com","Tl@123");
+        homePage.approveConnection("TL01");
+    test.pass("Approved By TL");
+        homePage = loginPg.login("spm01@veolia.com","Spm@123");
+        homePage.approveConnection("SPM");
+    test.pass("Approved By SPM");
+        homePage = loginPg.login("ne01@veolia.com","Ne@123");
+        homePage.approveConnection("NE");
+    test.pass("Approved By NE");
+        homePage = loginPg.login("zm01@veolia.com","Zm@123");
+        homePage.approveConnection("ZM");
+    test.pass("Approved By ZM");
+        homePage = loginPg.login("cso01@veolia.com","Cso@123");
+        homePage.approveConnection("CSO");
+    test.pass("Approved By CSO");
+        homePage = loginPg.login("spm01@veolia.com","Spm@123");
+        homePage.approveConnection("SPM");
+    test.pass("Approved By SPM");
+        homePage = loginPg.login("dl01@veolia.com","Dl@123");
+        homePage.approveConnection("DL");
+    test.pass("Approved By DL");
+//        driver.get(TestBase.prop.getProperty("url"));
+//        homePage = loginPg.login("swengp@gmail.com","Saltoris@123");
+//    homePage.approveConnection("Aditya");
+    test.pass("Payment done");
+    test.pass("Approved By ne");
+    test.pass("Approved By zm");
+    test.pass("Approved By CSO");
+    test.pass("Approved By spm");
+    extent.flush();
+//            driver.get(TestBase.prop.getProperty("admin_url"));
+//        homePage = loginPg.login("ne01@veolia.com","Ne@123");
+//         homePage.approveConnection("NE01");
+//        homePage = loginPg.login("zm01@veolia.com","Zm@123");
+//        homePage.approveConnection("ZM");
+//        homePage = loginPg.login("cso01@veolia.com","Cso@123");
+//        homePage.approveConnection("CSO");
+//        homePage = loginPg.login("spm01@veolia.com","Spm@123");
+//        homePage.approveConnection("SPM");
+    }
+}
